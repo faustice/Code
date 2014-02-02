b8e1188e3c7b76d2c3dde7eacc868c1c83bd16e9
faustice <Saffron_aurora@hotmail.com>
2014-02-02 14:36:25 -0500

    Lab 3

---
+++ Lab_3.ino
@@ -0,0 +1,78 @@
+// Code taken from Analog Read Serial, Fade and Calibration examples
+// Saffron Aurora, February 2nd, 2014
+// Physical Computing, Kate Hartman 
+
+// These constants won't change:
+const int sensorPin = A0;    // pin that the sensor is attached to
+const int ledPin = 9;        // pin that the LED is attached to
+
+// variables:
+int sensorValue = 0;         // the sensor value
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+int led = 9;           // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+
+
+void setup() {
+  // turn on LED to signal the start of the calibration period:
+  pinMode(13, OUTPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(13, HIGH);
+  Serial.begin(9600);
+
+  // calibrate during the first five seconds 
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
+
+  // signal the end of the calibration period
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  
+    Serial.println(sensorValue);
+  delay(1);        // delay in between reads for stability
+  
+  // read the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  // apply the calibration to the sensor reading
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
+
+  // in case the sensor value is outside the range seen during calibration
+  sensorValue = constrain(sensorValue, 0, 255);
+
+  // fade the LED using the calibrated value:
+  analogWrite(ledPin, sensorValue);
+  
+  
+  
+  
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);    
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+  
+  if (sensorValue == 0) { delay(5); }
+  else {  delay(100);   }
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }        
+  
